f6563660f2fe159618f1804b51c0f0cc039b2874
Peddakasu <127223914+Peddakasu@users.noreply.github.com>
2025-08-28 15:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 import pandas as pd
 import requests
+import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
